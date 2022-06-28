1c1cfc46d514b8c5aa8d0642a2cdd721f68c94dd
James Yeap <james.yeap@accenture.com>
2022-06-28 19:39:16 +0800

    Add Controller

---
+++ src/main/java/com/bakkcover/controller/Controller.java
@@ -0,0 +1,32 @@
+package com.bakkcover.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping
+    public String hello() {
+        InetAddress ip;
+        String hostname = "";
+        String hostip = "";
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            hostip = ip.toString();
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }
+        return "<html>Hello! <br>" +
+                "Your current IP address : " + hostip + "<br>" +
+                "Your current Hostname : " + hostname + "<br>"
+                + "</html>";
+    }
+}
