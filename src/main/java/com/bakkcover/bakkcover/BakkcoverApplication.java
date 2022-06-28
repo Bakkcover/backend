c08d2b96bb14111cab80d71d2c527f3f234958d1
James Yeap <james.yeap@accenture.com>
2022-06-28 17:11:05 +0800

    Add Spring Boot files

---
+++ src/main/java/com/bakkcover/bakkcover/BakkcoverApplication.java
@@ -0,0 +1,13 @@
+package com.bakkcover.bakkcover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakkcoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakkcoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakkcover/bakkcover/BakkcoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakkcover.bakkcover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakkcoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
